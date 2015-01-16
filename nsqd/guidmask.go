180f7900fb78944f5a6eecbf87c7306562ecdc5f
Stephen Searles <stephen.searles@shipwire.com>
2015-01-16 14:47:38 -0800

    added a filtering mechanism for guids

---
+++ nsqd/guidmask.go
@@ -0,0 +1,74 @@
+package nsqd
+
+import "time"
+
+type guids []guid
+
+type guidFilter interface {
+	match(g guid) bool
+}
+
+type workerFilter struct {
+	id int64
+}
+
+func (w workerFilter) match(g guid) bool {
+	g = g << workerIdShift
+	g = g >> timestampShift
+	worker := g >> sequencBits
+	return worker == w.id
+}
+
+type timestampFilter struct {
+	earliest, latest time.Time
+}
+
+func (t timestampFilter) match(g guid) bool {
+	g = g >> timestampShift
+	at := time.Unix(0, g)
+	return at.Before(t.latest) && at.After(t.earliest)
+}
+
+type sequenceFilter struct {
+	earliest, latest int64
+}
+
+func (s sequenceFilter) match(g guid) bool {
+	g = g << timestampShift
+	g = g << workerIdShift
+	g = g >> workerIdShift
+	g = g >> timestampShift
+	return g > s.earliest && g < s.latest
+}
+
+type andFilter []guidFilter
+
+func (fs andFilter) match(g guid) bool {
+	for _, f := range fs {
+		if !f.match(g) {
+			return false
+		}
+	}
+	return true
+}
+
+type orFilter []guidFilter
+
+func (fs orFilter) match(g guid) bool {
+	for _, f := range fs {
+		if f.match(g) {
+			return true
+		}
+	}
+	return false
+}
+
+func (gs guids) Filter(f guidFilter) guids {
+	matched := make(guids, 0)
+	for _, g := range gs {
+		if f.match(g) {
+			matched = append(matched, g)
+		}
+	}
+	return matched
+}
