d6f8015b6a4f169a6a8738e9679c841c22a0b164
Stephen Searles <stephen.searles@shipwire.com>
2015-01-21 15:26:54 -0800

    renaming flag

--- nsqd/options.go
+++ nsqd/options.go
@@ -22,7 +22,7 @@ type nsqdOptions struct {
 	AuthHTTPAddresses      []string `flag:"auth-http-address" cfg:"auth_http_addresses"`
 
 	GossipAddress     string   `flag:"gossip-address"`
-	SeedNodeAddresses []string `flag:"seed-node-address"`
+	SeedNodeAddresses []string `flag:"gossip-seed-address"`
 
 	// diskqueue options
 	DataPath        string        `flag:"data-path"`
